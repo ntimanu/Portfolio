a052c849ca7eec67f47a68dade9aeccfaaa70cae
HussenYesufAlli <hussenyesufalli@gmail.com>
2023-05-23 21:15:22 +0800

    Update Js linter config

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const toggleButton = document.getElementsByClassName("hamburger")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-  toggleButton.classList.toggle("hidden");
+const toggleButton = document.getElementsByClassName('hamburger')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+  toggleButton.classList.toggle('hidden');
 });
 
-document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    toggleButton.classList.remove("active");
-    navbarLinks.classList.remove("active");
-  })
-);
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  toggleButton.classList.remove('active');
+  navbarLinks.classList.remove('active');
+}));
